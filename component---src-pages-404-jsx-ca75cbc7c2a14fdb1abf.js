d7bc5cf42fbe7ea95974d79e80eecc14bf224e67
RohitGeddam-tech <RohitGeddam-tech@users.noreply.github.com>
2021-06-30 11:50:21 +0000

    deployed via Gatsby Publish Action 🎩 for a90be0343dac0af384b2c4c15a29d0cbfe7ef1af

---
+++ component---src-pages-404-jsx-ca75cbc7c2a14fdb1abf.js
@@ -0,0 +1,2 @@
+(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[256],{1657:function(t,e,n){"use strict";n.r(e);var u=n(7294),a=n(750),l=n(1277);e.default=function(){return u.createElement(a.Z,null,u.createElement(l.Z,{title:"404: Not found"}),u.createElement("h1",null,"404: Not Found"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-jsx-ca75cbc7c2a14fdb1abf.js.map
---
+++ component---src-pages-page-2-jsx-1d2e5ff036eefedc238c.js
@@ -0,0 +1,2 @@
+(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[497],{8534:function(e,t,a){"use strict";a.r(t);var l=a(7294),n=a(5444),r=a(750),c=a(1277);t.default=function(){return l.createElement(r.Z,null,l.createElement(c.Z,{title:"Page two"}),l.createElement("h1",null,"Hi from the second page"),l.createElement("p",null,"Welcome to page 2"),l.createElement(n.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-jsx-1d2e5ff036eefedc238c.js.map
